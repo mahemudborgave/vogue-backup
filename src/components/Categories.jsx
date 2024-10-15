090a4f9d8a238ae82438f4f0373fb1e217ef4027
Sandesh Shinde <sandeshshinde7047@gmail.com>
2024-10-15 19:01:11 +0530

    Categories added

---
+++ src/components/Categories.jsx
@@ -0,0 +1,49 @@
+import React from "react";
+
+export default function Categories() {
+    let categories = [
+        { img: "4568144.webp", title: "Shirt" },
+        { img: "4568147.webp", title: "Pants" },
+        { img: "4568146.webp", title: "Jacket" },
+        { img: "4568144.webp", title: "Shoes" },
+        { img: "4568147.webp", title: "Hat" },
+        { img: "4568146.webp", title: "Bags" },
+        { img: "4568144.webp", title: "Accessories" },
+        { img: "4568147.webp", title: "Watch" },
+        { img: "4568147.webp", title: "Glasses" }
+      ];
+
+  return (
+    <>
+      <div className="md:mt-20 mt-28">
+        <div className="text-center">
+          <h1 className="text-4xl font-bold text-dark-green tracking-[.25em]">
+            CATEGORY
+          </h1>
+        </div>
+
+        {/* Grid of Circles */}
+        <div className="grid grid-cols-3  sm:grid-cols-2 md:grid-cols-5 md:gap-10 gap-2 justify-items-center md:mt-16 mt-10">
+          {categories.map((category, index) => (
+            <div className="flex flex-col justify-center items-center tracking-[.10em] font-semibold">
+
+            <div
+              key={index}
+              className="bg-light-green md:w-40 md:h-40 md:m-0 m-4 flex justify-center items-center rounded-full overflow-hidden"
+            >
+              <img
+                src={category.img}
+                alt={category.title}
+                className="object-cover w-full h-full"
+              />
+            </div>
+
+            <h3 className="md:mt-5">{category.title}</h3>
+            </div>
+            
+          ))}
+        </div>
+      </div>
+    </>
+  );
+}
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,14 @@
 import React, { useState } from 'react';
 import Navbar from '../components/Navbar';
 import MainSlider from "../components/MainSlider";
+import Categories from '../components/Categories';
 function Home() { 
     return (
         <>
             <div className='bg-gray-100 h-full'>
             <Navbar/>
             <MainSlider/>
+            <Categories/>
             </div>
         </>
     );
